2a1072f46652175e3df12467338ecef58aab5d92
Prashant Gupta <68388206+PrashantGuptaa@users.noreply.github.com>
2020-11-07 22:44:57 +0530

    Clock

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,23 @@ class App extends Component {
     constructor(props){
         super(props);
         this.state = {
-        time: new Date().toLocaleTimeString()
+        time: new Date().toLocaleTimeString("en-US", {
+            hour: "numeric",
+            minute: "numeric",
+            second: "numeric",
+            hour12: true
+          })
         }
       }
       
       updateTime(){
       this.setState({
-        time: new Date().toLocaleTimeString()
+        time: new Date().toLocaleTimeString("en-US", {
+            hour: "numeric",
+            minute: "numeric",
+            second: "numeric",
+            hour12: true
+          })
       })
       }
       
