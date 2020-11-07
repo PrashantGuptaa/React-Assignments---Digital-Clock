5c64e88bab0e8ec817ebdfc0bd5539a96f53cbfa
Prashant Gupta <68388206+PrashantGuptaa@users.noreply.github.com>
2020-11-07 22:55:35 +0530

    clock

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,44 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-        time: new Date().toLocaleTimeString("en-US", {
-            hour: "numeric",
-            minute: "numeric",
-            second: "numeric",
-            hour12: true
-          })
-        }
-      }
-      
-      updateTime(){
-      this.setState({
-        time: new Date().toLocaleTimeString("en-US", {
-            hour: "numeric",
-            minute: "numeric",
-            second: "numeric",
-            hour12: true
-          })
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date().toLocaleTimeString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        hour12: true
       })
-      }
-      
-      componentDidMount(){
-      this.intervalID = setInterval(() =>{
-        this.updateTime();
-      },1000)
-      }
-      
-      
-      
-      componentWillUnmount(){
-      clearInterval(this.intervalID);
-      }
-      
-      render(){
-        return(
-      <div className='clock'>
-      <h3 id='time'> {this.state.time}</h3>
-      
+    };
+  }
+  tick() {
+    this.setState({
+      time: new Date().toLocaleTimeString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        hour12: true
+      })
+    });
+  }
+
+  componentDidMount() {
+    this.intervalID = setInterval(() => this.tick(), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="Clock">
+          <h3 id="time">{`${this.state.time}`}</h3>
+        </div>
       </div>
-        )
-      };
+    );
+  }
 }
-
-
 
 export default App;
