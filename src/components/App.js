3db8cf2164ebca958bff5ec723292dc975bf8390
Prashant Gupta <68388206+PrashantGuptaa@users.noreply.github.com>
2020-11-07 22:40:47 +0530

    Clock

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,41 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    render() {
-
+    constructor(props){
+        super(props);
+        this.state = {
+        time: new Date().toLocaleTimeString()
+        }
+      }
+      
+      updateTime(){
+      this.setState({
+        time: new Date().toLocaleTimeString()
+      })
+      }
+      
+      componentDidMount(){
+      console.log("Clock componentDidMount");
+      this.intervalID = setInterval(() =>{
+        this.updateTime();
+      },1000)
+      }
+      
+      
+      
+      componentWillUnmount(){
+        console.log("Clock componentWillUnmount");
+      clearInterval(this.intervalID);
+      }
+      
+      render(){
         return(
-            <>
-               
-            </>
+      <div className='clock'>
+      <div id='time'> {this.state.time}</div>
+      
+      </div>
         )
-    }
+      };
 }
 
 
